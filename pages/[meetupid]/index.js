47d91993a0de82f83ef337604e96d9e02ef4374b
Sridharrajkumar <sridharrajkumar47@gmail.com>
2023-11-20 12:20:06 +0530

    getstaticprops and path

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -11,5 +11,45 @@ const MeetUpdetails = () => {
   );
 };
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupid:'m1',
+        }
+      },
+      {
+        params: {
+          meetupid:'m2',
+        }
+      },
+
+    ],
+  }
+}
+
+
+
+export async function getStaticProps(context) {
+
+  const meetupId=context.params.meetupid;
+
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        title: "A fourth meetup",
+        id: meetupId,
+        image:"https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+        address:"No.1/e subramainyan street chennai",
+        description:"This is a dummy meetup"
+      }
+    },
+
+  }
+}
+
 export default MeetUpdetails
 
--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,56 @@
-import MeetupList from '../components/meetups/MeetupList'
+import { useEffect, useState } from "react";
+import MeetupList from "../components/meetups/MeetupList";
 
 const Dummy_Meetups = [
-    {
-        id: 'm1',
-        title: 'A first meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
-        address: 'No.1/e subramainyan street chennai',
-        description: "This is a dummy meetup",
-    },
-    {
-        id: 'm2',
-        title: 'A second meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
-        address: 'No.1/e subramainyan street chennai',
-        description: "This is a dummy meetup",
-    },
-    {
-        id: 'm3',
-        title: 'A third meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
-        address: 'No.1/e subramainyan street chennai',
-        description: "This is a dummy meetup",
-    },
-    {
-        id: 'm4',
-        title: 'A fourth meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
-        address: 'No.1/e subramainyan street chennai',
-        description: "This is a dummy meetup",
-    }
-]
+  {
+    id: "m1",
+    title: "A first meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "No.1/e subramainyan street chennai",
+    description: "This is a dummy meetup",
+  },
+  {
+    id: "m2",
+    title: "A second meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "No.1/e subramainyan street chennai",
+    description: "This is a dummy meetup",
+  },
+  {
+    id: "m3",
+    title: "A third meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "No.1/e subramainyan street chennai",
+    description: "This is a dummy meetup",
+  },
+  {
+    id: "m4",
+    title: "A fourth meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "No.1/e subramainyan street chennai",
+    description: "This is a dummy meetup",
+  },
+];
 
-const HomePage = () => {
-    
-    return <MeetupList meetups={Dummy_Meetups} />
+const HomePage = (props) => {
+  
+
+  return <MeetupList meetups={props.meetups} />;
+};
+
+export async function getStaticProps() {
+  // Call an API to fetch data for the homepage.
+
+  return {
+    props: {
+      meetups: Dummy_Meetups,
+    },
+    revalidate: 50,
+  };
 }
 
-export default HomePage
+export default HomePage;
